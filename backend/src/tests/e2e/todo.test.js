6845d88c9f5a201b027eaa4b18885949715c531a
WesleyVitor <wesleydemorais@outlook.com.br>
2022-04-28 10:46:23 -0300

    test: When dont persiste in db and return all todos how array

--- backend/src/tests/e2e/todo.test.js
+++ backend/src/tests/e2e/todo.test.js
@@ -1,4 +1,4 @@
-import { test, describe, expect, it } from "@jest/globals";
+import { test, describe, expect, jest } from "@jest/globals";
 import request from "supertest";
 import app from "../../server.js";
 describe("TODO API", () => {
@@ -26,12 +26,40 @@ describe("TODO API", () => {
         });
     });
   });
-  describe("POST /todo --> When todo not is created", () => {
-    test.todo("should return status 404");
+  describe("POST /todo --> When todo is not created", () => {
+    test("should return status 500", () => {
+      return request(app)
+        .post("/todo")
+        .send({ title: "Fazer algo" })
+        .expect(500)
+        .then((response) => {
+          expect(response.body.msg).toBe("db Error");
+        });
+    });
   });
   describe("GET /todo/ --> When todos exist", () => {
-    test.todo("should return status 200");
-    test.todo("should return a array of todo");
+    test("should return status 200", () => {
+      return request(app)
+        .get("/todo")
+        .expect("Content-Type", /json/)
+        .expect(200);
+    });
+    test("should return a array of todo", async () => {
+      return request(app)
+        .get("/todo")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          expect(response.body).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                title: expect.any(String),
+                description: expect.any(String),
+              }),
+            ])
+          );
+        });
+    });
   });
   describe("GET /todo --> When todos not exists", () => {
     test.todo("should return status 404");
