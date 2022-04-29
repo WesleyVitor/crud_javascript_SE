f93cfc7858c2e16ac99f22bea73315e969291abb
WesleyVitor <wesleydemorais@outlook.com.br>
2022-04-29 14:32:21 -0300

    feat: Página de listar os todos

--- frontend/src/components/listTodo.jsx
+++ frontend/src/components/listTodo.jsx
@@ -1,7 +1,39 @@
+import { useEffect, useState } from "react";
+import api from "../service/api";
+import { Li } from "./ListTodoStyle";
 const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    api.get("/todo").then((response) => {
+      setTodos(response.data);
+    });
+  }, [todos]);
   return (
     <main>
-      <h2>Hello Todos</h2>
+      <ul>
+        {todos.map((todo, index) => {
+          if (!todo.done) {
+            return (
+              <Li key={index}>
+                <div>
+                  <p>
+                    {" "}
+                    <span>Título:</span>
+                    {todo.title}
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    {" "}
+                    <span>Descrição:</span>
+                    {todo.description}
+                  </p>
+                </div>
+              </Li>
+            );
+          }
+        })}
+      </ul>
     </main>
   );
 };
